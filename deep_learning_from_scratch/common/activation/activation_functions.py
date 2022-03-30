30dbaf1f7eff914ada0e4949a08ba508bfb7a196
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2022-03-31 08:27:43 +0900

    completed day2

--- deep_learning_from_scratch/common/activation/activation_functions.py
+++ deep_learning_from_scratch/common/activation/activation_functions.py
@@ -3,8 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def step_function(x):
-    return
+def identity_function(x):
+    return x
+
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
