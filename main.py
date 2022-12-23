8244c212e135fb8917133e45b7176cea1cf1fe81
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2022-12-23 09:51:37 +0900

    finished cp3-2

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import numpy as np
+import scipy
 
 def pprint(n: int):
     for _ in range(n):
@@ -5,5 +7,11 @@ def pprint(n: int):
 
 
 if __name__ == '__main__':
-    pprint(10)
+    # pprint(10)
+
+    nums = np.array([[0, 1], [2, 3]])
+    print(scipy.cov(nums))
+
+
+
 
